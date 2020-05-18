152747af60ba762b35c54f1552232a46dedb1bc8
necopotts <sulryishiki@gmail.com>
2020-05-18 19:09:38 +0700

    Update add-drop_query.sql
    updated add-drop_query

--- Database/add-drop_query.sql
+++ Database/add-drop_query.sql
@@ -3,7 +3,7 @@
 
 --- Class id, subject code, name subject, credit, weekday, time, room, staff name
 
-SELECT A2.class_id, c.subject_code, c.name_subject, c.credit, A2.weekday, A2.time, A2.room_number FROM Course c
+SELECT A2.class_id, c.subject_code, c.name_subject, c.credit, A2.weekday, A2.time, A2.room_number, A2.name_staff FROM Course c
 INNER JOIN (SELECT cl.class_id,cl.subject_code,cl.weekday,cl.time,cl.room_number,A1.name_staff FROM Classes cl,(SELECT t.class_id, s.name_staff FROM Teach t INNER JOIN Staff s ON t.id_staff = s.id_staff) AS A1 
 			WHERE cl.class_id = A1.class_id) AS A2
 ON A2.subject_code = c.subject_code
